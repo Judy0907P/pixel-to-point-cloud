8e396b82eec5f1122cf3ba75152fa4585e3abed2
judy0907p <jiayizhai0907@gmail.com>
2025-10-19 22:51:11 -0700

    week2 task completed

--- oaf_vision_3d/lens_model.py
+++ oaf_vision_3d/lens_model.py
@@ -190,11 +190,10 @@ def _distort_pixels(
         + distortion_coefficients.k4 * r2
         + distortion_coefficients.k5 * r4
         + distortion_coefficients.k6 * r6
-
     )
-    radial_coefficient = radial_coefficient_numerator/radial_coefficient_denomenator
+    radial_coefficient = radial_coefficient_numerator / radial_coefficient_denomenator
     radial_distortion = normalized_pixels * radial_coefficient[..., None]
-    
+
     # tangential_distortion
     two_xy = 2 * normalized_pixels[..., 0] * normalized_pixels[..., 1]
     r2_2x2 = r2 + 2 * normalized_pixels[..., 0] ** 2
@@ -208,16 +207,50 @@ def _distort_pixels(
         axis=-1,
     )
     # thin_prism_distortion
-    thin_prism_distortion_x = distortion_coefficients.s1*r2 + distortion_coefficients.s2*r4
-    thin_prism_distortion_y = distortion_coefficients.s3*r2 + distortion_coefficients.s4*r4
+    thin_prism_distortion_x = (
+        distortion_coefficients.s1 * r2 + distortion_coefficients.s2 * r4
+    )
+    thin_prism_distortion_y = (
+        distortion_coefficients.s3 * r2 + distortion_coefficients.s4 * r4
+    )
     thin_prism_distortion = np.stack(
-        (
-           thin_prism_distortion_x,  thin_prism_distortion_y
-        ),
+        (thin_prism_distortion_x, thin_prism_distortion_y),
         axis=-1,
     )
 
-    return radial_distortion + tangential_distortion + thin_prism_distortion
+    before_tilt = radial_distortion + tangential_distortion + thin_prism_distortion
+    tilt_matrix = np.array(
+        [
+            [np.cos(distortion_coefficients.tau_x), 0, 0],
+            [
+                -np.sin(distortion_coefficients.tau_x)
+                * np.sin(distortion_coefficients.tau_y),
+                np.cos(distortion_coefficients.tau_y),
+                0,
+            ],
+            [
+                np.sin(distortion_coefficients.tau_y),
+                -np.sin(distortion_coefficients.tau_x)
+                * np.cos(distortion_coefficients.tau_y),
+                np.cos(distortion_coefficients.tau_x)
+                * np.cos(distortion_coefficients.tau_y),
+            ],
+        ]
+    )
+    extra_ones = np.ones_like(before_tilt[..., 0])
+    before_tilt_with_ones = np.stack(
+        (before_tilt[..., 0], before_tilt[..., 1], extra_ones),
+        axis=-1,
+    )
+    after_tilt = before_tilt_with_ones @ tilt_matrix.T
+    after_tilt_x = after_tilt[..., 0]
+    after_tilt_y = after_tilt[..., 1]
+    after_tilt_z = after_tilt[..., 2]
+    final_pixels = np.stack(
+        (after_tilt_x / after_tilt_z, after_tilt_y / after_tilt_z),
+        axis=-1,
+    )
+    return final_pixels
 
 
 def _undistort_pixels(
